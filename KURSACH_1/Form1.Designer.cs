dbf1f6fc738ce5db6d513df21d0e2642d70f6e0e
Еремей Миронов <yerem01@mail.ru>
2022-10-08 23:04:37 +0300

    123

--- KURSACH_1/Form1.Designer.cs
+++ KURSACH_1/Form1.Designer.cs
@@ -37,14 +37,18 @@ namespace KURSACH_1
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
@@ -54,10 +58,10 @@ namespace KURSACH_1
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(287, 233);
+            this.chart1.Size = new System.Drawing.Size(360, 352);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
@@ -65,13 +69,13 @@ namespace KURSACH_1
             // 
             chartArea2.Name = "ChartArea1";
             this.chart2.ChartAreas.Add(chartArea2);
-            this.chart2.Location = new System.Drawing.Point(305, 12);
+            this.chart2.Location = new System.Drawing.Point(378, 12);
             this.chart2.Name = "chart2";
             series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
             series2.Name = "Series1";
             this.chart2.Series.Add(series2);
-            this.chart2.Size = new System.Drawing.Size(287, 233);
+            this.chart2.Size = new System.Drawing.Size(388, 352);
             this.chart2.TabIndex = 1;
             this.chart2.Text = "chart2";
             // 
@@ -79,13 +83,13 @@ namespace KURSACH_1
             // 
             chartArea3.Name = "ChartArea1";
             this.chart3.ChartAreas.Add(chartArea3);
-            this.chart3.Location = new System.Drawing.Point(12, 251);
+            this.chart3.Location = new System.Drawing.Point(12, 370);
             this.chart3.Name = "chart3";
             series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
             series3.Name = "Series1";
             this.chart3.Series.Add(series3);
-            this.chart3.Size = new System.Drawing.Size(287, 233);
+            this.chart3.Size = new System.Drawing.Size(360, 347);
             this.chart3.TabIndex = 2;
             this.chart3.Text = "chart3";
             // 
@@ -93,21 +97,36 @@ namespace KURSACH_1
             // 
             chartArea4.Name = "ChartArea1";
             this.chart4.ChartAreas.Add(chartArea4);
-            this.chart4.Location = new System.Drawing.Point(305, 251);
+            this.chart4.Location = new System.Drawing.Point(378, 370);
             this.chart4.Name = "chart4";
             series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
             series4.Name = "Series1";
             this.chart4.Series.Add(series4);
-            this.chart4.Size = new System.Drawing.Size(287, 233);
+            this.chart4.Size = new System.Drawing.Size(388, 347);
             this.chart4.TabIndex = 3;
             this.chart4.Text = "chart4";
+            // 
+            // chart5
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            this.chart5.Location = new System.Drawing.Point(748, 103);
+            this.chart5.Name = "chart5";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Name = "Series1";
+            this.chart5.Series.Add(series5);
+            this.chart5.Size = new System.Drawing.Size(597, 494);
+            this.chart5.TabIndex = 4;
+            this.chart5.Text = "chart5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 529);
+            this.ClientSize = new System.Drawing.Size(1357, 767);
+            this.Controls.Add(this.chart5);
             this.Controls.Add(this.chart4);
             this.Controls.Add(this.chart3);
             this.Controls.Add(this.chart2);
@@ -118,6 +137,7 @@ namespace KURSACH_1
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -128,6 +148,7 @@ namespace KURSACH_1
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
     }
 }
 
--- KURSACH_1/Form1.cs
+++ KURSACH_1/Form1.cs
@@ -12,7 +12,8 @@ namespace KURSACH_1
 {
     public partial class Form1 : Form
     {
-        double find_C(double T)
+
+        double find_C(double f, int m)
         {
             double A1 = 40000;
             double A2 = 35000;
@@ -21,12 +22,20 @@ namespace KURSACH_1
             double E2 = 68000;
             double E3 = 70000;
             double R = 8.31;
-            double K1 = A1 * Math.Exp(-E1 / (R * T));
-            double K2 = A2 * Math.Exp(-E2 / (R * T));
-            double K3 = A3 * Math.Exp(-E3 / (R * T));
             double C1 = 0.4;
             double C2 = 0.3;
-            double C3;
+            double T = 300;
+            double T0 = 300;
+            double Kt = 5000;
+            double Q1 = 81000;
+            double Q2 = 48600;
+            double p = 810;
+            double Ct = 1200;
+            double Tt = 285;
+            double K1;
+            double K2;
+            double K3;
+            double C3 = 0;
             double C5 = 0;
             double C10 = C1;
             double C20 = C2;
@@ -34,33 +43,63 @@ namespace KURSACH_1
             double C50 = 0;
 
 
-            for (int t = 0; t < 1800; t++)
+            for (double t = 0; t < 1800; t++)
             {
-                C1 = C10 + (-K2 * C10 * C30 - 3 * K1 * C10 * C20 - 2 * K3 * C10 * C20) * t;
-                C2 = C20 + (-K1 * C10 * C20 - 4 * K1 * C10 * C20) * t;
-                C3 = C30 + (K1 * C10 * C20 - K2 * C10 * C30) * t;
-                C5 = C50 + (K2 * C10 * C30) * t;
-               
-                chart1.Series[0].Points.AddXY(t, C1);
-                chart2.Series[0].Points.AddXY(t, C2 );
-                chart3.Series[0].Points.AddXY(t, C3);
-                chart4.Series[0].Points.AddXY(t, C5 );
-                //Console.WriteLine(Math.Abs(C5));
+                K1 = A1 * Math.Exp(-E1 / (R * T));
+                K2 = A2 * Math.Exp(-E2 / (R * T));
+                K3 = A3 * Math.Exp(-E3 / (R * T));
+                C1 = C10 + (-K2 * C10 * C30 - 3 * K1 * C10 * C20 - 2 * K3 * C10 * C20) ;
+                C2 = C20 + (-K1 * C10 * C20 - 4 * K1 * C10 * C20) ;
+                C3 = C30 + (K1 * C10 * C20 - K2 * C10 * C30);
+                C5 = C50 + (K2 * C10 * C30) ;
+                T = T0 + ((-Kt * f * (T0 - Tt)) + K1 * C10 * C20 * Q1 * m / p + K2 * C10 * C30 * Q2 * m / p)/Ct/m ;
 
+
+                chart1.Series[0].Points.AddXY(t, C1);
+                chart2.Series[0].Points.AddXY(t, C2);
+                chart3.Series[0].Points.AddXY(t, C3);
+                chart4.Series[0].Points.AddXY(t, C5);
+                chart5.Series[0].Points.AddXY(t, T);
                 C10 = C1;
                 C20 = C2;
                 C30 = C3;
                 C50 = C5;
+                T0 = T;
 
-
+                //Console.WriteLine(Math.Abs(T));
             }
+           
             return C5;
         }
         public Form1()
         {
             InitializeComponent();
 
-            Console.WriteLine(find_C(400));
+
+            double n = 0;
+            double nf = 0;
+            double nm = 0;
+            
+            for (double f = 7; f < 12; f +=1)
+            {
+                for (int m = 2000; m < 3000; m+=10)
+                {
+                    //Console.WriteLine(find_C(f, m));
+                    if(find_C(f, m) > n)
+                    {
+                        n = find_C(f ,m);
+                        nf = f;
+                        nm = m;
+                       // Console.WriteLine("n = " + n);
+                    }
+
+                }
+            }
+                // find_C(7, 2000);
+            Console.WriteLine("n = "+ n);
+            Console.WriteLine("nf = " + nf);
+            Console.WriteLine("nm = " + nm);
+
         }
     }
 }
